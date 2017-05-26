e5c98f296c4513ba5024234f4576fcd9fc174dac
lor104 <lauren.rosentzveig@gmail.com>
2017-05-25 23:40:30 -0400

    set function to find the index of the card that was flipped, compare it to the second card that was flipped

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+  var clicks = 0; //keep track of number of clicks
+  var firstChoice; //first card clicked - will store the index
+  var secondChoice; //second card clicked - will store the index
+
   skills = []
   skills[0] = "images/ruby.png"
   skills[1] = "images/html.png"
@@ -21,15 +25,57 @@ $(document).ready(function() {
     var back_card = "<img src='"+ skills[i] +"' class='face back'></img>"
     $(front_card).appendTo(this_div)
     $(back_card).appendTo(this_div)
-      // $('<img>').attr("src", front_img).appendTo(this_div);
-      // $('<img>').attr("src", skills[i]).appendTo(this_div);
      }
 
   $('img').on('click', function(e) {
-      $(this).parent().toggleClass("flip");
+      if (clicks === 2) {
+        return;
+      } else if (clicks === 0) {
+        $(this).parent().toggleClass("flip");
+        // firstChoice = skills.indexOf($(this).attr("src"));
+        clicks++;
+        var div = $(this).parent()
+        var divClass = $(div).attr("class")
+        divClass = divClass.split(" ")
+        divClass = divClass[0]
+        var imageFind = "div." + divClass + " img.back"
+        console.log(divClass)
+        console.log(imageFind)
+        imageFind = $(imageFind).attr("src")
+        console.log(imageFind)
+        firstChoice = skills.indexOf(imageFind)
+        console.log(firstChoice)
+      } else {
+        $(this).parent().toggleClass("flip");
+        secondChoice = skills.indexOf($(this).attr("src"));
+        clicks++;
+        var div = $(this).parent()
+        var divClass = $(div).attr("class")
+        divClass = divClass.split(" ")
+        divClass = divClass[0]
+        var imageFind = "div." + divClass + " img.back"
+        console.log(divClass)
+        console.log(imageFind)
+        imageFind = $(imageFind).attr("src")
+        console.log(imageFind)
+        secondChoice = skills.indexOf(imageFind)
+        console.log(secondChoice)
+        // setInterval(checkMatch(firstChoice, secondChoice), 1000);
+      }
   })
 
-  // $('img.back').on('click', function(e) {
-  //     $(this).toggleClass("return");
-  // })
+  function checkMatch(firstChoice, secondChoice) {
+    if (firstChoice === secondChoice) {
+      //match
+    } else {
+      //reset
+      var divClassFirst = ".card" + firstChoice
+      console.log(divClassFirst)
+      var divClassSecond = ".card" + secondChoice
+      console.log(divClassSecond)
+      $(divClassFirst).toggleClass("flip");
+      $(divClassSecond).toggleClass("flip");
+      clicks = 0;
+    }
+  }
 });
